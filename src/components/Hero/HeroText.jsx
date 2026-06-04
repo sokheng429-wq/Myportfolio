17ee53a523c9503670e31000f339bc66afb1e130
sokheng429-wq <sokheng429@gmail.com>
2026-06-04 12:44:50 +0700

    Update project

--- src/components/Hero/HeroText.jsx
+++ src/components/Hero/HeroText.jsx
@@ -6,7 +6,7 @@ function HeroText () {
         <div className=" h-icon col-md-7 mx-auto col-lg-7 col-12">
         <h1 className="h1 display-5 fw-bold text-body-emphasis lh-1 mb-3">Aspiring Full-Stack Developer<span id="wave">&#x1F44B;&#x1F3FB;</span></h1>
             <p className="fs-5">
-            Hi, I'm THOEUN SOKHENG. A passionate Full-Stack Node.js Developer based in Phnom Penh, Cambodia. 📍
+            Hi, I'm THOEUN SOKHENG. A passionate FULL-STACK JAVA DEVELOPER based in Phnom Penh, Cambodia. 📍
             Currently looking for an internship or junior developer role to grow my
             skills in real-world projects.
             </p>
